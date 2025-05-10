f683d4dc59177d0fed56476cdaed78ec0aeaa6dd
samueleangel <samueleangel@gmail.com>
2025-05-10 17:32:07 +0200

    Fix

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,2 +1,3 @@
 #!/usr/bin/python3
-print("{}".format(", ".join(f"{i}{j}" for i in range(10) for j in range(i + 1, 10))))
+print("{}".format(", ".join(
+    f"{i}{j}" for i in range(10) for j in range(i + 1, 10))))
