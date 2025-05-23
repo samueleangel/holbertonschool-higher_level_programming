590b23d91a874c7f3401ece237f3242af49c6626
samueleangel <samueleangel@gmail.com>
2025-05-23 14:36:37 +0200

    Added a new module to dine a class named square, to initialize a new square instance with a private size attribute

---
+++ python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""This module defines a class Square."""
+
+Class Square:
+    """This class represents a square."""
+
+    def __init__(self, size):
+        """Initialize a new Square instance with a private size attribute."""
+        self.__size = size
