de6e9834565dc0152b6aa20ba30cd46a488aa52a
samueleangel <samueleangel@gmail.com>
2025-05-25 11:52:10 +0200

    Added square class with private attributes

---
+++ python-classes/3-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""This module defines a class Square."""
+
+
+class Square:
+    """Represents a square.
+
+    Attributes:
+        __size (int): The size of the square (must be >= 0).
+    """
+
+    def __init__(self, size=0):
+        """Initialize a new Square
+
+        Args:
+            size(int): The size of the square. Defaults to 0.
+
+            Raises:
+                TypeError: If size is not an integer.
+                ValueError: If size is less than 0.
+            """
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+
+            self.__size = size
+
+        def area(self):
+            """Returns the current square area."""
+            return self.__size ** 2
