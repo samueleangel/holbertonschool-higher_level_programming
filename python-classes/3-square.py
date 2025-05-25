7a794c79a47c256b9117eccfa4104108a5b91f4d
samueleangel <samueleangel@gmail.com>
2025-05-25 11:56:24 +0200

    fix

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -10,23 +10,22 @@ class Square:
     """
 
     def __init__(self, size=0):
-        """Initialize a new Square
+        """Initialize a new Square.
 
         Args:
-            size(int): The size of the square. Defaults to 0.
+            size (int): The size of the square (default is 0).
 
-            Raises:
-                TypeError: If size is not an integer.
-                ValueError: If size is less than 0.
-            """
-
+        Raises:
+            TypeError: If size is not an integer.
+            ValueError: If size is less than 0.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-            if size < 0:
-                raise ValueError("size must be >= 0")
+        if size < 0:
+            raise ValueError("size must be >= 0")
 
-            self.__size = size
+        self.__size = size
 
-        def area(self):
-            """Returns the current square area."""
-            return self.__size ** 2
+    def area(self):
+        """Returns the current square area."""
+        return self.__size ** 2
