ba1a7117a73fda8aa72b4eb1fde106ebb96c3913
samueleangel <samueleangel@gmail.com>
2025-06-07 01:06:33 +0200

    Added read file

---
+++ python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def read_file(filename=""):
+    """Reads a text file (UTF8) and prints it to stdout.
+
+    Args:
+        filename (str): The name of the file to read.
+    """
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")
