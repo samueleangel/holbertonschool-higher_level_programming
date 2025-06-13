b8f858ddc709f20512790c1a1deb1d59dc05b651
samueleangel <samueleangel@gmail.com>
2025-06-13 23:29:00 +0200

    Added requests.py

---
+++ restful-api/task_02_requests.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+import requests
+import csv
+
+
+def fetch_and_print_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+    print(f"Status Code: {response.status_code}")
+    if response.status_code == 200:
+        posts = response.json()
+        for post in posts:
+            print(post['title'])
+    else:
+        print("Failed to fetch posts.")
+
+
+def fetch_and_save_posts():
+    url = "https://jsonplaceholder.typicode.com/posts"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        posts = response.json()
+        data_to_write = [{'id': post['id'], 'title': post['title'], 'body':
+                         post['body']} for post in posts]
+
+        with open('posts.csv', mode='w', newline='', encoding='utf-8')
+        as csvfile:
+            fieldnames = ['id', 'title', 'body']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            writer.writerows(data_to_write)
+    else:
+        print("Failed to fetch posts.")
