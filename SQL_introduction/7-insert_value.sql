a0efbd4ab1e123c8b36b901be8b8b9d18f994821
samueleangel <samueleangel@gmail.com>
2025-06-29 12:50:25 +0200

    Fix

--- SQL_introduction/7-insert_value.sql
+++ SQL_introduction/7-insert_value.sql
@@ -1,4 +1,2 @@
 -- Inserts a new row into the table first_table.
-USE hbtn_0c_0;
-
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Best School");
