4cde53a804ada89233b8e1b7cfb4cd42f5a94d30
samueleangel <samueleangel@gmail.com>
2025-06-29 15:29:31 +0200

    Fix

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -15,4 +15,5 @@ if __name__ == "__main__":
 
     c = db.cursor()
     c.execute("SELECT * FROM states ORDER BY id ASC")
-    [print(state) for state in c.fetchall()]
+    for state in c.fetchall():
+        print(state)
