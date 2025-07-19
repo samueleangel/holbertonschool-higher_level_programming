30901ef7c44c7e967f461c24df38e93548bba817
samueleangel <samueleangel@gmail.com>
2025-07-19 17:50:02 +0200

    Scripts added

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,13 @@
+fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+  .then(response => response.json())
+  .then(data => {
+    const list = document.querySelector('#list_movies');
+    data.results.forEach(film => {
+      const li = document.createElement('li');
+      li.textContent = film.title;
+      list.appendChild(li);
+    });
+  })
+  .catch(error => {
+    console.error('Error fetching movies:', error);
+  });
